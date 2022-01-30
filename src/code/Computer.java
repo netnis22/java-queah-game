d2efb0debc8a7b38039a1c813b5a6af71bf54c35
netnis <netnis22@gmail.com>
2022-01-30 14:02:00 +0200

    ניסיון לתקן באג
    הבאג הוא שאני ה row and colom לא משתנה אחרי שהמחשב זז

--- src/code/Computer.java
+++ src/code/Computer.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.Scanner;
+
 public class Computer extends Players {
 
     public Computer(int player_color, String map) {
@@ -8,6 +10,11 @@ public class Computer extends Players {
 
     //return newRow,newColumn,previsRow,previsColumn, eatRow,eatColum, isEat- 1(yes)/0(no)
     public int[] move() {
+        // int test[]=new int[7];
+        // Scanner scanner = new Scanner(System.in);
+        // for(int i=0;i<7;i++){
+        //     test[i]= Integer.parseInt(scanner.nextLine());
+        // }
         int test[]= {2,4,2,3,0,0,0};
         return test;
     }
