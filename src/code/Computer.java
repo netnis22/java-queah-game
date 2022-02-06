91d0e5ca320a417e189287da5913dab9fbafb90d
netnis <netnis22@gmail.com>
2022-02-06 08:12:15 +0200

    ניסיון תיקון באג

--- src/code/Computer.java
+++ src/code/Computer.java
@@ -10,12 +10,12 @@ public class Computer extends Players {
 
     //return newRow,newColumn,previsRow,previsColumn, eatRow,eatColum, isEat- 1(yes)/0(no)
     public int[] move() {
-        // int test[]=new int[7];
-        // Scanner scanner = new Scanner(System.in);
-        // for(int i=0;i<7;i++){
-        //     test[i]= Integer.parseInt(scanner.nextLine());
-        // }
-        int test[]= {2,4,2,3,0,0,0};
+        int test[]=new int[7];
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0;i<7;i++){
+            test[i]= Integer.parseInt(scanner.nextLine());
+        }
+        // int test[]= {2,4,2,3,0,0,0};
         return test;
     }
 
